f59fed715b03c684ecd6f7ebebefb42ec40e3355
SMARCOCCIA <seb@gmail.fr>
2024-05-22 14:50:20 +0200

    Création des attaques

--- tpPokemon/src/tpPokemon/Main.java
+++ tpPokemon/src/tpPokemon/Main.java
@@ -3,7 +3,10 @@ package tpPokemon;
 public class Main {
 
 	public Main() {
-		// TODO Auto-generated constructor stub
+		Attaque aireDEau = new Attaque("Aire d'eau", EAU, 80);
+		Attaque acideMalique = new Attaque("Acide Malique", PLANTE, 80);
+		Attaque acrobatie  = new Attaque("Acrobatie", AIRE, 55);
+		Attaque appelAttaque = new Attaque("Appel Attaque", INSECT, 90);
 	}
 
 }
