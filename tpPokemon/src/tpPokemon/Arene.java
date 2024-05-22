4bdb77f035aa8e5b5e648bdec28b3f48147bd393
Ouerdani Sakina <ouerdanisakina@gmail.com>
2024-05-22 15:07:57 +0200

    ajout de modification

--- tpPokemon/src/tpPokemon/Arene.java
+++ tpPokemon/src/tpPokemon/Arene.java
@@ -4,6 +4,13 @@ public class Arene {
 
 	public Arene() {
 		// TODO Auto-generated constructor stub
+		
+	private final Pokemon pokemon1;
+	private final Pokemon pokemon2;
+	
+	private final Arene arene;
+	
+		
 	}
 
 }
